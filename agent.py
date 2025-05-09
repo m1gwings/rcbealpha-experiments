453ce6e7bc26a2e37ccf3ff05d1b0252bc796d54
Cristiano Migali <cristiano.migali@gmail.com>
2025-05-09 18:33:37 +0200

    Little refactoring

---
+++ agent.py
@@ -0,0 +1,228 @@
+from abc import ABC, abstractmethod
+import numpy as np
+import matplot2tikz
+
+from abc import ABC, abstractmethod
+
+class Agent(ABC):
+    def __init__(self, K, name):
+        self.K = K
+        self.name = name
+
+    @abstractmethod
+    def pull(self):
+        pass
+
+    @abstractmethod
+    def observe(self, reward):
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+class RandomAgent(Agent):
+    def __init__(self, K, seed):
+        super().__init__(K, 'Random')
+        self.seed = seed
+
+    def pull(self):
+        return self.rng.integers(1, self.K)
+
+    def observe(self, reward):
+        pass
+    
+    def reset(self):
+        self.rng = np.random.default_rng(seed=self.seed)
+
+class WindowedAgent(Agent):
+    def __init__(self, K, name):
+        super().__init__(K, name)
+
+    @abstractmethod
+    def _get_window_length(self):
+        pass
+
+    @abstractmethod
+    def _reset_window(self):
+        pass
+
+    def _update_window(self):
+        self.d += 1
+        if self.d == self._get_window_length() + 1:
+            self.w += 1
+            self.d = 1
+            self._reset_window()
+    
+    def reset(self):
+        self.w = 1
+        self.d = 1
+
+class BEAlphaAgent(WindowedAgent):
+    def __init__(self, K, alpha, sigma):
+        super().__init__(K, '$\\operatorname{BE}(\\alpha)$')
+        self.alpha = alpha
+        self.sigma = sigma
+
+    def pull(self):
+        self.last_pulled = min(self.B) + 1
+        return self.last_pulled
+
+    def observe(self, reward):
+        self.S[self.last_pulled - 1] += reward
+        self.B.remove(self.last_pulled - 1)
+
+        if not self.B:
+            self._eliminate_arms()
+
+        self._update_window()
+
+    def _get_window_length(self):
+        return int(np.ceil(self.w ** self.alpha))
+
+    def _eliminate_arms(self):
+        S_star = max([self.S[i] for i in self.A])
+        delta_w = self._get_window_length()
+        B_w = 2 * (1 + 2 * self.sigma * np.sqrt(delta_w * np.log(2 * self.K * delta_w)))
+        self.A = {i for i in self.A if self.S[i] + B_w >= S_star}
+        self.B = self.A.copy()
+
+    def _reset_window(self):
+        self.S = [0.0] * self.K
+        self.A = set(range(self.K))
+        self.B = self.A.copy()
+    
+    def reset(self):
+        super().reset()
+        self._reset_window()
+
+class Rexp3Agent(WindowedAgent):
+    def __init__(self, K, delta, gamma, seed):
+        super().__init__(K, 'Rexp3')
+        self.delta = delta
+        self.gamma = gamma
+        self.seed = seed
+
+    def pull(self):
+        self.last_pulled = self.rng.choice(np.arange(1, self.K + 1), p=self.p)
+        return self.last_pulled
+
+    def observe(self, reward):
+        self.weights[self.last_pulled - 1] *= np.exp(self.gamma / self.K * reward / self.p[self.last_pulled - 1])
+        self._update_p()
+        self._update_window()
+
+    def _get_window_length(self):
+        return self.delta
+
+    def _reset_window(self):
+        self.weights = np.ones(self.K)
+        self._update_p()
+
+    def _update_p(self):
+        self.p = (1 - self.gamma) * self.weights / np.sum(self.weights) + self.gamma / self.K
+    
+    @staticmethod
+    def tune_delta(T, K, V_T):
+        return int(np.ceil( (K * np.log(K)) ** (1/3) * (T/V_T) ** (2/3) ))
+
+    @staticmethod
+    def tune_gamma(T, K, V_T):
+        return min([1, np.sqrt(K * np.log(K) / (np.e - 1) / Rexp3Agent.tune_delta(T, K, V_T))])
+
+    def reset(self):
+        super().reset()
+        self.rng = np.random.default_rng(seed=self.seed)
+        self._reset_window()
+
+class RLessUCBAgent(Agent):
+    def __init__(self, K, epsilon, alpha, sigma, initial_capacity=2**20):
+        super().__init__(K, 'R-less-UCB')
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.sigma = sigma
+        self.rewards = np.full((self.K, initial_capacity), np.nan)
+
+    def pull(self):
+        self.last_pulled = np.argmax(self.ucbs) + 1
+        return self.last_pulled
+
+    def observe(self, reward):
+        if self.rewards_counts[self.last_pulled - 1] >= self.rewards.shape[1]:
+            self._expand_rewards()
+
+        self.rewards[self.last_pulled - 1, self.rewards_counts[self.last_pulled - 1]] = reward
+        self.rewards_counts[self.last_pulled - 1] += 1
+        
+        old_h = self.h[self.last_pulled-1]
+        self.h[self.last_pulled-1] = int(np.floor(self.epsilon * self.rewards_counts[self.last_pulled - 1]))
+        h = self.h[self.last_pulled-1]
+        n = self.rewards_counts[self.last_pulled-1]
+        old_reward = self.rewards[self.last_pulled-1, n-h-1]
+        old_oldish_reward = self.rewards[self.last_pulled-1, n-2*h]
+        old_old_reward = self.rewards[self.last_pulled-1, n-2*h-1]
+        if h == old_h:
+            self.a[self.last_pulled-1] += reward - old_reward
+            self.b[self.last_pulled-1] += old_reward - old_old_reward
+            self.c[self.last_pulled-1] += n*reward - (n-h)*old_reward
+            self.d[self.last_pulled-1] += n*old_reward - (n-h)*old_old_reward
+        else:
+            self.a[self.last_pulled-1] += reward
+            self.b[self.last_pulled-1] += old_oldish_reward
+            self.c[self.last_pulled-1] += n*reward
+            self.d[self.last_pulled-1] += (n-h)*old_oldish_reward + self.b[self.last_pulled-1]
+        self.t += 1
+
+        self._update_preds_ucbs()
+
+    def _expand_rewards(self):
+        new_capacity = self.rewards.shape[1] * 2
+        new_rewards = np.full((self.K, new_capacity), np.nan)
+        new_rewards[:, :self.rewards.shape[1]] = self.rewards
+        self.rewards = new_rewards
+
+    def _update_preds_ucbs(self):
+        self.preds[self.h == 0] = np.inf
+        self.ucbs[self.h == 0] = np.inf
+
+        self.preds[self.h > 0] = 1/self.h[self.h > 0] * (self.a[self.h > 0] + self.t*(self.a[self.h > 0] - self.b[self.h > 0])/self.h[self.h > 0] - (self.c[self.h > 0] - self.d[self.h > 0])/self.h[self.h > 0])
+
+        delta_t = self.t ** (-self.alpha)
+        self.ucbs[self.h > 0] = self.preds[self.h > 0] + self.sigma * (self.t - self.rewards_counts[self.h > 0] + self.h[self.h > 0] - 1) * np.sqrt(10 * np.log(1/delta_t) / (self.h[self.h > 0] ** 3))
+    
+    def reset(self):
+        self.rewards_counts = np.zeros(self.K, dtype=int)
+        self.h = np.zeros(self.K, dtype=int)
+        self.preds = np.full(self.K, np.inf)
+        self.ucbs = np.full(self.K, np.inf)
+        self.a = np.zeros(self.K)
+        self.b = np.zeros(self.K)
+        self.c = np.zeros(self.K)
+        self.d = np.zeros(self.K)
+        self.t = 1
+
+class UCB1Agent(Agent):
+    def __init__(self, K, sigma):
+        super().__init__(K, 'UCB1')
+        self.sigma = sigma
+
+    def pull(self):
+        self.last_pulled = np.argmax(self.ucbs) + 1
+        return self.last_pulled
+
+    def observe(self, reward):
+        self.counts[self.last_pulled - 1] += 1
+        self.cumulative_rewards[self.last_pulled - 1] += reward
+
+        self.t += 1
+        self._update_ucbs()
+    
+    def _update_ucbs(self):
+        self.ucbs[self.counts == 0] = np.inf
+        self.ucbs[self.counts > 0] = self.cumulative_rewards[self.counts > 0] / self.counts[self.counts > 0] + self.sigma * np.sqrt(4 * np.log(self.t) / self.counts[self.counts > 0])
+    
+    def reset(self):
+        self.counts = np.zeros(self.K, dtype=int)
+        self.cumulative_rewards = np.zeros(self.K)
+        self.ucbs = np.full(self.K, np.inf)
+        self.t = 1
---
+++ instance.py
@@ -0,0 +1,168 @@
+from abc import ABC, abstractmethod
+import numpy as np
+import matplotlib.pyplot as plt
+import matplot2tikz
+
+OUT_FOLDER = 'out'
+
+class Instance(ABC):
+    def __init__(self, K):
+        self.K = K
+
+    @abstractmethod
+    def next_payoffs(self):
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+def plot_instance(plot_name, instance, T, max_points=100):
+    instance.reset()
+
+    payoffs = np.zeros((T, instance.K))
+
+    for t in range(T):
+        payoffs[t] = instance.next_payoffs()
+    x_plt = np.linspace(0, T, min([T, max_points]), endpoint=False, dtype=int)
+    plt.figure(figsize=(12, 6))
+    for i in range(instance.K):
+        plt.plot(x_plt+1, payoffs[x_plt, i], label=f'Arm {i+1}', alpha=0.7)
+
+    plt.xlabel("Round ($t$)")
+    plt.ylabel("Payoffs")
+    plt.title("Instance Payoff Curves Over Time")
+    plt.grid(True, linestyle='--', alpha=0.5)
+    if instance.K <= 15:
+        plt.legend()
+    plt.tight_layout()
+
+    matplot2tikz.save(f'{OUT_FOLDER}/{plot_name}.tex')
+
+    plt.show()
+
+class RandomInstance(Instance):
+    def __init__(self, K, seed):
+        super().__init__(K)
+        self.seed = seed
+
+    def next_payoffs(self):
+        return tuple(self.rng.uniform(0, 1) for _ in range(self.K))
+
+    def reset(self):
+        self.rng = np.random.default_rng(seed=self.seed)
+
+class ExponentialInstance(Instance):
+    def __init__(self, K, T, seed):
+        super().__init__(K)
+        
+        rng = np.random.default_rng(seed=seed)
+        self.c = 1 - rng.random(K)
+        self.a = 1 - rng.random(K)
+        self.T = T
+
+    def next_payoffs(self):
+        payoffs = self.c * (1 - np.exp(-self.a * self.t * 10 / self.T))
+        self.t += 1
+        return payoffs
+
+    def reset(self):
+        self.t = 1
+
+class PolynomialInstance(Instance):
+    def __init__(self, K, T, seed):
+        super().__init__(K)
+        
+        rng = np.random.default_rng(seed=seed)
+        self.c = 1 - rng.random(K)
+        self.a = 1 - rng.random(K)
+        self.rho = 1 - rng.random(K)
+        self.b = rng.exponential(K)
+        self.T = T
+
+    def next_payoffs(self):
+        payoffs = self.c * (1 - self.b * ((1000 * self.t / self.T) + self.b ** ( 1. / self.rho )) ** (-self.rho) )
+        self.t += 1
+        return payoffs
+
+    def reset(self):
+        self.t = 1
+
+class LineInstance(Instance):
+    def __init__(self, K, T, seed):
+        super().__init__(K)
+
+        rng = np.random.default_rng(seed=seed)
+        endpoints = rng.uniform(0.0, 1.0, size=(K, 2))
+        self.initial = np.minimum(endpoints[:, 0], endpoints[:, 1])
+        self.final = np.maximum(endpoints[:, 0], endpoints[:, 1])
+        self.T = T
+    
+    def next_payoffs(self):
+        alpha = (self.t - 1) / (self.T - 1)
+        payoffs = self.initial * (1 - alpha) + self.final * alpha
+        self.t += 1
+        return payoffs
+
+    def reset(self):
+        self.t = 1
+
+class StationaryInstance(Instance):
+    def __init__(self, K, seed):
+        super().__init__(K)
+
+        rng = np.random.default_rng(seed=seed)
+        self.payoffs = rng.uniform(low=0.0, high=1.0, size=K)
+    
+    def next_payoffs(self):
+        return self.payoffs
+
+    def reset(self):
+        pass
+
+class WindowedInstance(Instance):
+    @abstractmethod
+    def _get_window_length(self):
+        pass
+
+    def _update_window(self):
+        self.d += 1
+        if self.d == self._get_window_length() + 1:
+            self.w += 1
+            self.d = 1
+            self._reset_window()
+    
+    def reset(self):
+        self.w = 1
+        self.d = 1
+
+class LowerBoundInstance(WindowedInstance):
+    def __init__(self, K, T, V_T, seed):
+        super().__init__(K)
+        self.K = K
+        self.delta_c = 1/3
+        self.m_0 = (1-2*self.delta_c) * min([1, V_T]) / 2 / T
+        self.N_c = np.ceil(T**(1/5) * K**(-1/5) * min([1, V_T])**(2/5))
+        self.big_delta_c = int(np.ceil(T/self.N_c))
+        self.seed = seed
+    
+    def next_payoffs(self):
+        m_high = (2 * self.N_c - (2*self.w - 2)) * self.m_0 / 2 / self.N_c
+        m_mid  = (2 * self.N_c - (2*self.w - 1)) * self.m_0 / 2 / self.N_c
+        m_low  = (2 * self.N_c - 2*self.w)       * self.m_0 / 2 / self.N_c
+        self.payoffs[np.arange(self.K) != self.modified_arm-1] += m_mid
+        self.payoffs[self.modified_arm-1] += m_high if self.d <= float(self.big_delta_c)/2 else m_low
+        self._update_window()
+        return self.payoffs
+
+    def _get_window_length(self):
+        return self.big_delta_c
+    
+    def _reset_window(self):
+        self.modified_arm = self.rng.integers(1, self.K)
+
+    def reset(self):
+        super().reset()
+        self.payoffs = np.full(self.K, self.delta_c)
+        self.rng = np.random.default_rng(seed=self.seed)
+        self._reset_window()
